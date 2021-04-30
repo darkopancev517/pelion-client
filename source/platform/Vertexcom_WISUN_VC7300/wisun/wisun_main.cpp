96a8ec9b33e8fecdd11a803f712c0f563fc9dcff
darkopancev517 <igededarkopancev@gmail.com>
2021-04-30 11:30:51 +0800

    simplify main application

--- source/platform/Vertexcom_WISUN_VC7300/wisun/wisun_main.cpp
+++ source/platform/Vertexcom_WISUN_VC7300/wisun/wisun_main.cpp
@@ -1,5 +1,9 @@
 #include <stdint.h>
 
+#include "contiki.h"
+#include "contiki-net.h"
+#include "contiki-lib.h"
+
 // Needed for PRIu64 on FreeRTOS
 #include <stdio.h>
 // Note: this macro is needed on armcc to get the the limit macros like UINT16_MAX
@@ -45,13 +49,118 @@
 
 extern "C" int wisun_main_system_init(void);
 
+#if 0
+static void main_arm_pelion_application(void);
+
+
+// Pointer to mbedClient, used for calling close function.
+static SimpleM2MClient *client;
+
+void main_arm_pelion_application(void)
+{
+    // Initialize storage
+    if (mcc_platform_storage_init() != 0) {
+        printf("Failed to initialize storage\r\n");
+        return;
+    }
+
+    // Initialize platform-specific components
+    if (mcc_platform_init() != 0) {
+        printf("ERROR - platform_init() failed!\r\n");
+        return;
+    }
+
+    // Print some statistics of the object sizes and their heap memory consumption.
+    // NOTE: This *must* be done before creating MbedCloudClient, as the statistic calculation
+    // creates and deletes M2MSecurity and M2MDevice singleton objects, which are also used by
+    // the MbedCloudClient.
+#ifdef MEMORY_TESTS_HEAP
+    print_m2mobject_stats();
+#endif
+
+    // SimpleClient is used for registering and unregistering resources to a server.
+    SimpleM2MClient mbedClient;
+
+    // Save pointer to mbedClient so that other functions can access it.
+    client = &mbedClient;
+
+    /*
+     * Pre-initialize network stack and client library.
+     *
+     * Specifically for nanostack mesh networks on Mbed OS platform it is important to initialize
+     * the components in correct order to avoid out-of-memory issues in Device Management Client initialization.
+     * The order for these use cases should be:
+     * 1. Initialize network stack using `nsapi_create_stack()` (Mbed OS only). // Implemented in `mcc_platform_interface_init()`.
+     * 2. Initialize Device Management Client using `init()`.                   // Implemented in `mbedClient.init()`.
+     * 3. Connect to network interface using 'connect()`.                       // Implemented in `mcc_platform_interface_connect()`.
+     * 4. Connect Device Management Client to service using `setup()`.          // Implemented in `mbedClient.register_and_connect)`.
+     */
+    (void) mcc_platform_interface_init();
+    mbedClient.init();
+
+#if 0
+    // application_init() runs the following initializations:
+    //  1. platform initialization
+    //  2. print memory statistics if MEMORY_TESTS_HEAP is defined
+    //  3. FCC initialization.
+    if (!application_init()) {
+        printf("Initialization failed, exiting application!\r\n");
+        return;
+    }
+#endif
+
+#if defined MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER &&\
+ (MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER == 1)
+    printf("Configuring Interface\r\n");
+    if (network_manager.reg_and_config_iface(NetworkInterface::get_default_instance()) != NM_ERROR_NONE) {
+        printf("Failed to register and configure Interface\r\n");
+        return;
+    }
+#endif
+
+    // Print platform information
+    mcc_platform_sw_build_info();
+
+    // Initialize network
+    int timeout_ms = 1000;
+    while (-1 == mcc_platform_interface_connect()){
+        // Will try to connect using mcc_platform_interface_connect forever. 
+        // wait timeout is always doubled
+        printf("Network connect failed. Try again after %d milliseconds.\n",timeout_ms);
+        mcc_platform_do_wait(timeout_ms);
+        timeout_ms *= 2;
+    }
+    printf("Network initialized, registering...\r\n");    
+
+#ifdef MEMORY_TESTS_HEAP
+    printf("Client initialized\r\n");
+    print_heap_stats();
+#endif
+
+#if 0
+#if defined MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER &&\
+ (MBED_CONF_MBED_CLOUD_CLIENT_NETWORK_MANAGER == 1)
+    network_manager.create_resource(mbedClient.get_m2m_obj_list());
+#endif
+
+    mbedClient.register_and_connect();
+#endif
+}
+#endif
+
+
 int main(void)
 {
     wisun_main_system_init();
 
-    // should not reach here.
+    //main_arm_pelion_application();
 
-    while (1);
+    while (1)
+    {
+        thread_sleep();
+    }
+
+    // should not reach here.
 
     return 0;
 }
--- source/platform/Vertexcom_WISUN_VC7300/wisun/wisun_system_init.c
+++ source/platform/Vertexcom_WISUN_VC7300/wisun/wisun_system_init.c
@@ -21,28 +21,6 @@
 #include "misc/sys_hook.h"
 #include "misc/shell_commands.h"
 
-#if (CONFIG_RF_TEST_MODE==1)
-#include "misc/ber.h"
-#endif
-
-#if (CONFIG_TRAV_TASK==1)
-#include "misc/trav.h"
-#endif
-
-#if (CONFIG_DEBUG_PRCSSVNT==1)
-#include "misc/prcssvnt.h"
-#endif
-
-#if (CONFIG_DEBUG_ROMCHK==1)
-#include "misc/romchk.h"
-#endif
-
-#include "misc/topcmd.h"
-
-#if (CONFIG_DEBUG_CENTRG==1)
-#include "misc/centrgchk.h"
-#endif
-
 #include "hal/board.h"
 
 #include "contiki.h"
@@ -101,32 +79,10 @@
 
 extern void vcsystem_init(void);
 
-#if (CONFIG_DEBUG_MAIN_LOOP_STALL==1)
-int m_main_loop_cnt=0;
-#endif
 int wisun_main_system_init(void)
 {
     board_init();
-#if CONFIG_WISUN_DUAL_MODE
-    // RF TX LED ON
-    hal_gpio_output_init(WISUN_DUAL_MODE_DEMO_RFTXLED_PORT, WISUN_DUAL_MODE_DEMO_RFTXLED_PIN);
-    hal_gpio_low(WISUN_DUAL_MODE_DEMO_RFTXLED_PORT, WISUN_DUAL_MODE_DEMO_RFTXLED_PIN);
 
-    // PLC TX LED ON
-    hal_gpio_output_init(WISUN_DUAL_MODE_DEMO_PLCTXLED_PORT, WISUN_DUAL_MODE_DEMO_PLCTXLED_PIN);
-    hal_gpio_low(WISUN_DUAL_MODE_DEMO_PLCTXLED_PORT, WISUN_DUAL_MODE_DEMO_PLCTXLED_PIN);
-    hal_misc_usec_delay(5000);
-
-    // RF TX LED OFF
-    hal_gpio_output_init(WISUN_DUAL_MODE_DEMO_RFTXLED_PORT, WISUN_DUAL_MODE_DEMO_RFTXLED_PIN);
-    hal_gpio_high(WISUN_DUAL_MODE_DEMO_RFTXLED_PORT, WISUN_DUAL_MODE_DEMO_RFTXLED_PIN);
-
-    // PLC TX LED OFF
-    hal_gpio_output_init(WISUN_DUAL_MODE_DEMO_PLCTXLED_PORT, WISUN_DUAL_MODE_DEMO_PLCTXLED_PIN);
-    hal_gpio_high(WISUN_DUAL_MODE_DEMO_PLCTXLED_PORT, WISUN_DUAL_MODE_DEMO_PLCTXLED_PIN);
-#else
-
-#if !defined(MCU_USE_V8530)
     // RX LED ON
     hal_gpio_output_init(RADIO_HOST_RXLED_PORT, RADIO_HOST_RXLED_PIN);
     hal_gpio_low(RADIO_HOST_RXLED_PORT, RADIO_HOST_RXLED_PIN);
@@ -143,9 +99,7 @@ int wisun_main_system_init(void)
     // TX LED OFF
     hal_gpio_output_init(RADIO_HOST_TXLED_PORT, RADIO_HOST_TXLED_PIN);
     hal_gpio_high(RADIO_HOST_TXLED_PORT, RADIO_HOST_TXLED_PIN);
-#endif
 
-#endif
 #if defined(CONFIG_WDT_EN)
     printf("Watchdog enabled\n");
     hal_wdt_set_period_2_secs();
@@ -155,67 +109,8 @@ int wisun_main_system_init(void)
 
     vcshell_init();
 
-    #if (CONFIG_TRAV_TASK==1)
-        trav_init();
-    #endif
-
-#if (CONFIG_DEBUG_PRCSSVNT==1)
-    prcssvnt_init();
-#endif
-    while (1) {
-        #ifndef CONFIG_BUILD_WITH_VCRTOS
-        uint8_t processList = 0;
-        #endif
-        do {
-            #if (CONFIG_RF_TEST_MODE==1)
-                if (ber_rf_test_mode()){
-                    extern void vcshell_task(void);
-                    ber_task();
-                    #if (CONFIG_TRAV_TASK==1)
-                        trav_task(TRAV_REASON_MAIN);
-                    #endif
-                    vcshell_task();
-                    #if defined(CONFIG_WDT_EN)
-                        hal_wdt_clear();
-                    #endif
-                }else{
-                    #ifndef CONFIG_BUILD_WITH_VCRTOS
-                    processList = process_run();
-                    #endif
-                }
-            #else
-                #ifndef CONFIG_BUILD_WITH_VCRTOS
-                processList = process_run();
-                #endif
-            #endif
-            #if (CONFIG_DEBUG_ROMCHK==1)
-                rom_check();
-            #endif
-            #if (CONFIG_DEBUG_CENTRG==1)
-                centrg_check();
-            #endif
-            #if (CONFIG_DEBUG_PHY_IRQ_STATUS_FIX==1)
-                extern void phy_fix_irq_chk();
-                phy_fix_irq_chk();
-            #endif
-            #if !defined(CONFIG_SYSTEM_bare) && defined(CONFIG_DEBUG_SEND_UDP_APP) && !defined(CONFIG_WISUN_ROOT)
-                extern void udpsnd_task();
-                udpsnd_task();
-            #endif
-            #if (CONFIG_DEBUG_MAIN_LOOP_STALL==1)
-                m_main_loop_cnt++;
-            #endif
-                extern void vcradio_alive_check(void);
-                vcradio_alive_check();
-            #ifdef CONFIG_BUILD_WITH_VCRTOS
-            thread_sleep();
-            #endif
-#ifdef CONFIG_BUILD_WITH_VCRTOS
-        } while (1);
-#else
-        } while (processList > 0);
-#endif
-    }
+    extern void vcradio_alive_check(void);
+    vcradio_alive_check();
 
     return 0;
 }
